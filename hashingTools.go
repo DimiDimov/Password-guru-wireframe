3cd43ea553ce7e671651ebb8520258d965bcbea9
DimiDimov <ddimov@uw.edu>
2017-02-27 22:22:24 -0800

    Create hashingTools.go

---
+++ hashingTools.go
@@ -0,0 +1,9 @@
+//This is the code that does proper password storage
+
+//generate a cryptographic random number for a salt
+
+//add a salt value to it
+
+//hash the combination of both these value
+
+//pass these values to a database for storage
