6727c65f202d760a024f027c0f61d6e5830b62d5
DimiDimov <ddimov@uw.edu>
2017-02-27 22:20:25 -0800

    Delete test.java

--- test.java
+++
@@ -1,9 +0,0 @@
-//Example java code that makes two factor authentication.
-
-//google authenticator code 
-
-//twilio sms authentication code
-
-//Authy code
-
-//blizzard one button authenticator code
