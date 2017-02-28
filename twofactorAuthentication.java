bd1e1ccacb9de959c714ab1d4d304c920c3959b7
DimiDimov <ddimov@uw.edu>
2017-02-27 22:20:11 -0800

    Create twofactorAuthentication.java

---
+++ twofactorAuthentication.java
@@ -0,0 +1,9 @@
+//Example java code that makes two factor authentication.
+
+//google authenticator code 
+
+//twilio sms authentication code
+
+//Authy code
+
+//blizzard one button authenticator code
