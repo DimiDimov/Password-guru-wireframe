032b3656ad7d1393cdadf9d30aa149427eaf0be0
DimiDimov <ddimov@uw.edu>
2017-02-27 22:26:41 -0800

    Create FrontEndPassStrengthCheck.js

---
+++ FrontEndPassStrengthCheck.js
@@ -0,0 +1,11 @@
+//code that takes in the password from the html
+
+//code that checks the strength based on length
+
+//code that checks the strength based on use of common words
+
+//code that checks the strength based on other common passwords
+
+//code that generates a number to summarize the strength
+
+//code that makes suggestions for a variation of suggested password 
