577adee68da2db413f20966d870eefa5335e09e0
DimiDimov <ddimov@uw.edu>
2017-02-27 22:19:30 -0800

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,9 @@
-test
+//Example java code that makes two factor authentication.
+
+//google authenticator code 
+
+//twilio sms authentication code
+
+//Authy code
+
+//blizzard one button authenticator code
