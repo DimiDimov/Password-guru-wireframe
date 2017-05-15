e1413586c362a4c814f5ac57287b7f7038c135d7
Dimi Dimov <Dimi Dimov>
2017-05-14 23:34:40 -0700

    basic structure

--- FrontEndPassStrengthCheck.js
+++ FrontEndPassStrengthCheck.js
@@ -1,11 +1,150 @@
-//code that takes in the password from the html
+//code that loads the common words and common passwords file
 
-//code that checks the strength based on length
+$(document).ready(function() {
+    fetch('10k_most_common.txt').then(function(response) {
+        if (response.ok) {
+            response.text().then(function(text) {
 
-//code that checks the strength based on use of common words
+                commonpasswords = text.split('\n');
+            });
+        } else {
+            console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
+        }
+    })
 
-//code that checks the strength based on other common passwords
+    fetch('google-10000-english-usa.txt').then(function(response) {
+        if (response.ok) {
+            response.text().then(function(text) {
 
-//code that generates a number to summarize the strength
+                commonwords = text.split('\n');
+            });
+        } else {
+            console.log('Network request for products.json failed with response ' + response.status + ': ' + response.statusText);
+        }
+    });
+});
 
-//code that makes suggestions for a variation of suggested password 
+
+function guruStrengthTest(username, password) {
+
+
+  //begining with a simple length test
+  if (pass1.length > 9) {
+    lengthscore = 1;
+  }
+
+   else if (pass1.length <= 9 && pass1.length >=8) {
+    lengthscore = 0.5;
+  }
+  else {
+    lengthscore = 0;
+  }
+
+
+  //now checking if the words in the password are in a common words list
+  if (commonpasswords.includes(pass1) && pass1 != "") {
+        commonpasswordscore = -10;
+  } else {
+        commonpasswordscore = 2;
+  }
+
+  if (commonwords.includes(pass1) && pass1 != "") {
+        commonwordscore = 0;
+  } else {
+        commonwordscore = 1;
+  }
+
+
+  var containslist = [""];
+
+  var i;
+  var j;
+  var alreadycontained = false;
+  var longest = true;
+  for (i = 0; i <= commonwords.length; i++) {
+    if(pass1.includes(commonwords[i]) && commonwords[i].length > 2) {
+      containslist.push(commonwords[i]);
+
+      /*
+       //trying not to put the user in double jeopardy, i.e. registering "too" and "tool" as two separate instances
+              //this only works if the shorter of the two instances is inputted first. But I'm dumb someone fix this
+              for (j = 0; j < containslist.length; j++) {
+
+         console.log(containslist[j]);
+                  console.log(commonwords[i]);
+                  if ((commonwords[i].includes(containslist[j]) || containslist[j].includes(commonwords[i])) && commonwords[i].length > containslist[j].length){
+                      containslist.splice(j, 1);
+          var longest = true;
+                  } else if (containslist[j].includes(commonwords[i]) || commonwords[i].includes(containslist[j])) {
+                      alreadycontained = true;
+          //containslist.push(commonwords[i]);
+          //j++;
+                  }
+
+        if(!alreadycontained){
+          containslist.push(commonwords[i]);
+        }
+        if(longest){
+          containslist.push(commonwords[i]);
+        }
+
+
+              }
+          }
+    */
+  }
+}
+var longest = true;
+var word;
+for(i = 0; i < containslist.length; i++){
+  longest = true;
+  word = containslist[i];
+  for (j = 0; j < containslist.length; j++){
+    if (containslist[j].includes(word)) {
+      longest = false;
+    }
+    //if we find a word that's included in our word and shorter
+    if (word.includes(containslist[j]) && (word.length > containslist[j].length)){
+      containslist.splice(j, 1);
+    }
+  }
+  if(!longest) {
+    containslist.splice(containslist.indexOf(word), 1);
+    i = 0;
+  }
+}
+
+
+  var commonwordcount = containslist.length;
+  console.log(containslist);
+  console.log(commonwordcount);
+      containslist = [""];
+
+      if(commonwordcount > 1) {
+          document.getElementById("picture4").src = "cross.jpg";
+          commonwordscore = 0;
+      } else if (commonwordcount == 1) {
+    document.getElementById("picture4").src = "cross.jpg";
+          commonwordscore = 0;
+      } else {
+    console.log("hit");
+    document.getElementById("picture4").src = "che.jpg";
+    commonwordscore++;
+      }
+  console.log(commonwordcount);
+      commonwordcount = 0;
+
+
+  totalscore = lengthscore + commonpasswordscore + commonwordscore;
+
+  if (pass1.length == 0) {
+    document.getElementById("picture1").src = "che.jpg";
+    totalscore = 0;
+  }
+
+  if(totalscore < 0) {
+    totalscore = 0;
+  }
+
+  return totalscore * 20;
+}
